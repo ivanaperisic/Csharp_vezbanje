023fbee8d165cd6f9982d22b36156ffaa52acf2d
Ivana <34-2018@ftn.edu.rs>
2022-01-31 20:23:42 +0100

    first commit

---
+++ Automobil.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Csharp_vezbanje
+{
+    class Automobil
+    {
+        public string marka;
+        public int godProizvodnje;
+        public decimal cena;
+        public string regBroj;
+
+        public void Unos()
+        {
+            Console.WriteLine("Unesite marku:");
+            this.marka = Console.ReadLine();
+            Console.WriteLine("Unesite godinu proizvodnje:");
+            this.godProizvodnje = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Unesite cenu:");
+            this.cena = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Unesite registarski broj");
+            this.regBroj = Console.ReadLine();
+        }
+            public void Ispis()
+        {
+            Console.WriteLine("Marka:{0}",this.marka);
+            Console.WriteLine("Godina proizvodnje:{0}",this.godProizvodnje);
+            Console.WriteLine("Cena:{0}",this.cena);
+            Console.WriteLine("Registarski broj:{0}",this.regBroj);
+           
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Csharp_vezbanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Automobil a = new Automobil();
+            Automobil b = new Automobil();
+
+            Console.WriteLine("Unesite podatke o 1. automobilu:");
+            a.Unos();
+            Console.WriteLine("Unesite podatke o 2. automobilu:");
+            b.Unos();
+            Console.WriteLine("Automobil koji ima vecu cenu:");
+            vecaCena(a, b);
+        }
+        static void vecaCena(Automobil a,Automobil b)
+        {
+            if (a.cena > b.cena)
+            {
+                a.Ispis();
+            }
+            else { 
+                b.Ispis();
+                 }
+
+        }
+
+
+
+    }
+}
